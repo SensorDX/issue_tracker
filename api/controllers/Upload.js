14dbddd390a667af79d89b58f0036821e5e35be3
Rene Midouin <renemidouin@gmail.com>
2018-08-01 07:22:32 -0400

    saving comment pictues to IBM Cloud Object Storage

--- api/controllers/Upload.js
+++ api/controllers/Upload.js
@@ -1,5 +1,29 @@
 //Libraries
 const path = require('path');
+var AWS = require('ibm-cos-sdk');
+var config = {
+    endpoint: 's3-api.us-geo.objectstorage.softlayer.net/',
+    apiKeyId: 'GcCbLdgTEGAMbTDV_qvGkdgQoFXqtc1P4lej7JnTcNGW',
+    ibmAuthEndpoint: 'iam.ng.bluemix.net/oidc/token',
+    serviceInstanceId: 'crn:v1:bluemix:public:cloud-object-storage:global:a/16a94a5aac6765350e3b23814baba84b:cfff3f7d-aa64-44b8-9361-56d507fcf62b::'
+};
+var cos = new AWS.S3(config);
+
+function uploadImage(bucketName, itemName, data) {
+    console.log(`Creating new item: ${itemName}`);
+    return cos.putObject({
+        Bucket: bucketName,
+        Key: itemName,
+				ACL: 'public-read',
+        Body: data
+    }).promise()
+    .then(() => {
+        console.log(`Item: ${itemName} created!`);
+    })
+    .catch((e) => {
+        console.log(`ERROR: ${e.code} - ${e.message}\n`);
+    });
+}
 
 module.exports = function(router) {
  //=====================
@@ -13,15 +37,11 @@ module.exports = function(router) {
 		}
 
 		const file = req.files.file;
+		console.log('this is the file', file);
 		const file_path = path.resolve('./src/assets/uploads/'+file.name);
-		const url = req.protocol + '://' + req.get('host') + '/assets/uploads/' + file.name;
-
-		file.mv(file_path, function(err) {
-			if (err) { 
-				return res.status(200).send({'success': false, 'message': err});
-			}
-			return res.status(200).send({'success': true, 'message': 'File uploaded!', 'url': url});
-		});
+		const url = 'https://tickets.s3-api.us-geo.objectstorage.softlayer.net/' + file.name;
+		uploadImage('tickets', file.name, file.data);
+		return res.status(200).send({'success': true, 'message': 'File uploaded!', 'url': url});
 	});
 
 }
--- server.js
+++ server.js
@@ -8,7 +8,61 @@ var fileUpload = require('express-fileupload');
 var nodemailer = require('nodemailer');
 var credentials = require('./config/settings.js');//require('./../credentials');
 var port = process.env.PORT || 3000;
-
+var AWS = require('ibm-cos-sdk');
+var config = {
+    endpoint: 's3-api.us-geo.objectstorage.softlayer.net/',
+    apiKeyId: 'GcCbLdgTEGAMbTDV_qvGkdgQoFXqtc1P4lej7JnTcNGW',
+    ibmAuthEndpoint: 'iam.ng.bluemix.net/oidc/token',
+    serviceInstanceId: 'crn:v1:bluemix:public:cloud-object-storage:global:a/16a94a5aac6765350e3b23814baba84b:cfff3f7d-aa64-44b8-9361-56d507fcf62b::'
+};
+var cos = new AWS.S3(config);
+console.log('this is cos', cos);
+function getBuckets() {
+	console.log('Retrieving list of buckets');
+	return cos.listBuckets()
+	.promise()
+	.then((data) => {
+			if (data.Buckets != null) {
+					for (var i = 0; i < data.Buckets.length; i++) {
+							console.log(`Bucket Name: ${data.Buckets[i].Name}`);
+					}
+			}
+	})
+	.catch((e) => {
+			console.log(`ERROR: ${e.code} - ${e.message}\n`);
+	});
+}
+function getBucketContents(bucketName) {
+    console.log(`Retrieving bucket contents from: ${bucketName}`);
+    return cos.listObjects(
+        {Bucket: bucketName},
+    ).promise()
+    .then((data) => {
+				console.log('getting bucket contents', data);
+        if (data != null && data.Contents != null) {
+            for (var i = 0; i < data.Contents.length; i++) {
+                var itemKey = data.Contents[i].Key;
+                var itemSize = data.Contents[i].Size;
+								var params = {Bucket: bucketName, Key: itemKey};
+                console.log(`Item: ${itemKey} (${itemSize} bytes).`)
+            }
+        }
+    })
+    .catch((e) => {
+        console.log(`ERROR: ${e.code} - ${e.message}\n`);
+    });
+}
+getBuckets();
+getBucketContents('tickets');
+/*
+client.getContainers(function(error, containers) {
+	if (error) {
+		console.log('error retrieving containers', error);
+	} else {
+		console.log('containers', containers);
+	}
+});
+*/
 var mailTransport = nodemailer.createTransport({
 	service: 'Gmail',
 	host: "smtp.gmail.com",
@@ -31,4 +85,5 @@ app.use('/', router);
 
 app.listen(port, function() {
 	console.log('Listening on port '+ port);
+	console.log('process.env', process.env);
 });
