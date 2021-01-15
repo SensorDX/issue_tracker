ae7342984b5de11f4e0bbb1cf86e532bb4a700e1
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-01-15 10:47:27 -0800

    got rid of hardcoded database

--- api/models/connections.js
+++ api/models/connections.js
@@ -1,12 +1,12 @@
 var variables = JSON.parse(process.env.VCAP_SERVICES || '{}')
+var db_1 = process.env.DB_1;
+var db_2 = process.env.DB_2;
 console.log('variables', variables);
 var mongoose = require('mongoose');
 // var opts = {uri_decode_auth: true};
 var opts = {useNewUrlParser: true, useUnifiedTopology: true};
 
 // Issue Tracker DB
-var db_1 =
-  'mongodb://tahmo:7db_p%40%24%24word@ds261570.mlab.com:61570/sensordx-tickets';
 var issue_tracker_conn = mongoose.createConnection(db_1, opts, function(
   err,
   response
@@ -16,8 +16,6 @@ var issue_tracker_conn = mongoose.createConnection(db_1, opts, function(
 });
 
 // Stations Info DB
-var db_2 =
-  'mongodb://tahmo_stations:7db_p%40%24%24word@ds023480.mlab.com:23480/ta_stations';
 var sites_conn = mongoose.createConnection(db_2, opts, function(err, response) {
   if (err) console.log('Fail to connect to ' + db_2);
   else console.log('Successfully connected to ' + db_2);
