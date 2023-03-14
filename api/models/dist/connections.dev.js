0129d09ce8a485b0f39ce077b9d2c1cc19b60085
sergeo <ansergeo12@gmail.com>
2023-03-14 10:28:22 +0300

    adding 3 reconnection atempts before restart

--- api/models/dist/connections.dev.js
+++ api/models/dist/connections.dev.js
@@ -1,68 +1,66 @@
-//option two
+"use strict";
 
-var variables = JSON.parse(process.env.VCAP_SERVICES || '{}')
+//option two
+var variables = JSON.parse(process.env.VCAP_SERVICES || '{}');
 var db_1 = process.env.DB_1;
 var db_2 = process.env.DB_2;
-var mongoose = require('mongoose');
-var opts = {useNewUrlParser: true, useUnifiedTopology: true};
 
-// Issue Tracker DB
-var issue_tracker_conn = mongoose.createConnection(db_1, opts, function(
-  err,
-  response
-) {
+var mongoose = require('mongoose');
+
+var opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}; // Issue Tracker DB
+
+var issue_tracker_conn = mongoose.createConnection(db_1, opts, function (err, response) {
   if (err) {
     console.log('Fail to connect to ' + db_1);
-  }
-  else {
-    console.log('Successfully connected to ' + db_1);
+  } else {
+    console.log('Successfully connected to ' + db_1); // Handle database disconnection
 
-    // Handle database disconnection
-    issue_tracker_conn.on('disconnected', function() {
+    issue_tracker_conn.on('disconnected', function () {
       console.log('Lost connection to Issue Tracker DB');
       reconnect(issue_tracker_conn, db_1, opts);
     });
   }
-});
+}); // Stations Info DB
 
-// Stations Info DB
-var sites_conn = mongoose.createConnection(db_2, opts, function(err, response) {
+var sites_conn = mongoose.createConnection(db_2, opts, function (err, response) {
   if (err) {
     console.log('Fail to connect to ' + db_2);
-  }
-  else {
-    console.log('Successfully connected to ' + db_2);
+  } else {
+    console.log('Successfully connected to ' + db_2); // Handle database disconnection
 
-    // Handle database disconnection
-    sites_conn.on('disconnected', function() {
+    sites_conn.on('disconnected', function () {
       console.log('Lost connection to Stations Info DB');
       reconnect(sites_conn, db_2, opts);
     });
   }
-});
+}); // Fault Inbox DB
 
-// Fault Inbox DB
 var fault_inbox_db = '% DB string goes here %';
 var fault_inbox_conn = {}; // Your Fault Inbox DB connction code goes here;
 
-function reconnect(conn, dbUrl, opts, attemptNum = 1) {
+function reconnect(conn, dbUrl, opts) {
+  var attemptNum = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 1;
+
   if (attemptNum > 3) {
     console.log('Failed to reconnect to ' + dbUrl + ' after 3 attempts');
-    setTimeout(function() {
+    setTimeout(function () {
       process.exit(1); // return non-zero to restart application
     }, 120000); // wait for 2 minutes before returning non-zero exit code
+
     return;
   }
-  
+
   console.log('Attempting to reconnect to ' + dbUrl + ', attempt #' + attemptNum);
   conn.close();
-  setTimeout(function() {
-    conn.open(dbUrl, opts, function(err) {
+  setTimeout(function () {
+    conn.open(dbUrl, opts, function (err) {
       if (err) {
         console.log('Reconnect attempt #' + attemptNum + ' to ' + dbUrl + ' failed');
         reconnect(conn, dbUrl, opts, attemptNum + 1);
-      }
-      else {
+      } else {
         console.log('Reconnected to ' + dbUrl + ' successfully');
       }
     });
@@ -70,13 +68,11 @@ function reconnect(conn, dbUrl, opts, attemptNum = 1) {
 }
 
 module.exports = {
-  issue_tracker_conn,
-  sites_conn,
-  fault_inbox_conn
-};
+  issue_tracker_conn: issue_tracker_conn,
+  sites_conn: sites_conn,
+  fault_inbox_conn: fault_inbox_conn
+}; //option one
 
-
-//option one
 /*
 var variables = JSON.parse(process.env.VCAP_SERVICES || '{}')
 var db_1 = process.env.DB_1;
@@ -134,8 +130,6 @@ module.exports = {
   fault_inbox_conn
 }; 
 */
-
-
 //Original code
 
 /* var variables = JSON.parse(process.env.VCAP_SERVICES || '{}')
@@ -170,3 +164,4 @@ module.exports = {
   sites_conn,
   fault_inbox_conn
 }; */
+//# sourceMappingURL=connections.dev.js.map
